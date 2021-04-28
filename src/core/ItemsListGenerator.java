7492c79e905d4df249fb91fdf3b0a8bde1af5be1
Daniel Pallesen <dpa107@cs18234kq.canterbury.ac.nz>
2021-04-28 13:42:02 +1200

    Merged ItemsListGenerator.java and WeaponsListGenerator.java

--- src/core/ItemsListGenerator.java
+++ src/core/ItemsListGenerator.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
- * This class generates and ArrayList of in-game items using the parameters specified in items.txt.
+ * This class generates and ArrayList of in-game items using the parameters specified in items.txt and weapons.txt.
  * @author Dillon Pike, Daniel Pallesen
  * @version 24 April 2021
  */
@@ -15,14 +15,54 @@ public class ItemsListGenerator {
 	 * Returns an ArrayList of in-game items 
 	 * @return ArrrayList of Item objects
 	 */
-	public static ArrayList<Item> generate() {
+	public static ArrayList<Item> generateItem() {
 		ArrayList<Item> itemList = new ArrayList<Item>();
 		Scanner itemData = null;
 		
 		try {
-			itemData = new Scanner(new File("game-parameters\\items.txt"));
+			itemData = new Scanner(new File("game-parameters/items.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Items data file not found!");
+			e.printStackTrace();
+		}
+		
+		itemData.useDelimiter(";");
+		
+		// skip first line in file
+		itemData.nextLine();
+		
+		// generate object for each line and add to ArrayList
+		while(itemData.hasNext()) {
+			String name = itemData.next();
+			//System.out.println(name);
+			String desc = itemData.next();
+			//System.out.println(desc);
+			int size = itemData.nextInt();
+			//System.out.println(size);
+			int value = itemData.nextInt();
+			//System.out.println(value);
+			Item newItem = new Item(name, desc, size, value);
+			//System.out.println(newItem);
+			itemList.add(newItem);
+		}
+		
+		
+		return itemList;
+		
+	}
+	
+	/** 
+	 * Returns an ArrayList of in-game weapons 
+	 * @return ArrrayList of Item objects
+	 */
+	public static ArrayList<Item> generateWeapon() {
+		ArrayList<Item> itemList = new ArrayList<Item>();
+		Scanner itemData = null;
+		
+		try {
+			itemData = new Scanner(new File("game-parameters/weapons.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Weapons data file not found!");
 			e.printStackTrace();
 		}
 		
@@ -37,8 +77,9 @@ public class ItemsListGenerator {
 			String desc = itemData.next();
 			int size = itemData.nextInt();
 			int value = itemData.nextInt();
-			//Item newItem = new Item(name, desc, size, value);
-			Item newItem = new Item(name, desc, size);
+			int shots = itemData.nextInt();
+			int damage = itemData.nextInt();
+			Item newItem = new Weapon(name, desc, size, value, shots, damage);
 			itemList.add(newItem);
 		}
 		
@@ -47,8 +88,11 @@ public class ItemsListGenerator {
 		
 	}
 	public static void main(String[] args) {
+		/*File testFile = new File("");
+	    String currentPath = testFile.getAbsolutePath();
+	    System.out.println("current path is: " + currentPath);*/
 		// Run generator
-		System.out.println(ItemsListGenerator.generate());
+		System.out.println(ItemsListGenerator.generateItem());
 	}
 
 }
