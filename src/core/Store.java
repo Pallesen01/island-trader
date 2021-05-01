4d2adf440822803601d89af20383acf51376d3d3
Daniel Pallesen <dpa107@uclive.ac.nz>
2021-05-01 21:36:18 +1200

    Added methods to generate buying and selling list

--- src/core/Store.java
+++ src/core/Store.java
@@ -1,5 +1,6 @@
 package core;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.HashMap;
 
 /**
@@ -12,29 +13,37 @@ public class Store {
 	final private double BUY_MODIFIER = 0.75;
 	final private double VARIATION_MODIFIER = 0.4;
 	
+	private Random randomGenerator = new Random();
 	private HashMap<Item, Integer> buys = new HashMap<Item, Integer>();
 	private HashMap<Item, Integer> sells = new HashMap<Item, Integer>();
 	
 	/**
 	 * Constructs hash maps of item and price pairs for each item the store buys and sells.
-	 * @param buys items the store buys
-	 * @param sells items the store sells
 	 */
-	public Store(ArrayList<Item> buys, ArrayList<Item> sells) {
-		for (Item item : buys) {
-			int basePrice = item.getBasePrice();
-			double minPrice = (basePrice + basePrice * VARIATION_MODIFIER) * BUY_MODIFIER;
-			double maxPrice = (basePrice - basePrice * VARIATION_MODIFIER) * BUY_MODIFIER;
-			int price = (int) ((Math.random() * (maxPrice - minPrice)) + minPrice);
+	public Store() {
+		this.generateBuys();
+		this.generateSells();
+		/*for (Item item : buys) {
+			int price = randomizePrice(item);
 			this.buys.put(item, price);
 		}
 		for (Item item : sells) {
-			int basePrice = item.getBasePrice();
-			double minPrice = basePrice + basePrice * VARIATION_MODIFIER;
-			double maxPrice = basePrice - basePrice * VARIATION_MODIFIER;
-			int price = (int) ((Math.random() * (maxPrice - minPrice)) + minPrice);
+			int price = randomizePrice(item);
 			this.sells.put(item, price);
-		}
+		}*/
+	}
+	
+	/**
+	 * Generates random price for an item within a range
+	 * @param item
+	 * @return Returns price for item
+	 */
+	private int randomizePrice(Item item) {
+		int basePrice = item.getBasePrice();
+		double minPrice = (basePrice + basePrice * VARIATION_MODIFIER) * BUY_MODIFIER;
+		double maxPrice = (basePrice - basePrice * VARIATION_MODIFIER) * BUY_MODIFIER;
+		int price = (int) ((Math.random() * (maxPrice - minPrice)) + minPrice);
+		return price;
 	}
 	
 	/**
@@ -43,5 +52,41 @@ public class Store {
 	 */
 	public HashMap<Item, Integer> getBuys() {
 		return buys;
+	}
+	/**
+	 * Returns hash map of item and price pairs for each item the store sells.
+	 * @return 
+	 */
+	public HashMap<Item, Integer> getSells() {
+		return sells;
+	}
+	
+	/**
+	 * Generates a random list of items bought by the store
+	 */
+	public void generateBuys() {
+		HashMap<Item, Integer> newBuys = new HashMap<Item, Integer>();
+		ArrayList<Item> allItems = ItemsListGenerator.generateItem();
+		for (int i=0;i<10; i++) {
+			Item newItem = allItems.get(randomGenerator.nextInt(allItems.size()));
+			int price = randomizePrice(newItem);
+			newBuys.put(newItem, price);
+		}
+		buys = newBuys;
+		
+	}
+	/**
+	 * Generates a random list of items sold by the store
+	 */
+	public void generateSells() {
+		HashMap<Item, Integer> newSells = new HashMap<Item, Integer>();
+		ArrayList<Item> allItems = ItemsListGenerator.generateItem();
+		for (int i=0;i<10; i++) {
+			Item newItem = allItems.get(randomGenerator.nextInt(allItems.size()));
+			int price = randomizePrice(newItem);
+			newSells.put(newItem, price);
+		}
+		sells = newSells;
+		
 	}
 }
